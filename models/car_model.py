2f5cb55678a87756c567914838a02fe677275e94
Nikhil Kumar <nikhil.kumar@targetintegration.com>
2024-09-03 13:43:23 +0530

    Completed the basic functionality with the record of the car, cars owner and borrower

--- models/car_model.py
+++ models/car_model.py
@@ -1,70 +1,83 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from datetime import datetime
 
 class Car(models.Model):
-    _name = "car.management"
-    _description = "Car Management"
-    _rec_name = "model"
+        _name = "car.management"
+        _description = "Car Management"
+        _rec_name = "model"
 
-    #Car name
-    model = fields.Char(string="Car Model", required=True)
+        #Car name
+        model = fields.Char(string="Car Model", required=True)
 
-    #image of the car
-    image = fields.Image(string="Image")
+        #image of the car
+        image = fields.Image(string="Image")
 
-    # Original owner
-    owner_id = fields.Many2one('car.owner', string="Owner")
-    #owners email
-    owner_id_email = fields.Char(string="Owner's Email", related="owner_id.email")
+        # Original owner
+        owner_id = fields.Many2one('car.owner', string="Owner", required=True)
+        #owners email
+        owner_id_email = fields.Char(string="Owner's Email", related="owner_id.email")
 
-    # Renter of the car
-    borrower_id = fields.Many2one('borrower.details', string="Borrower")
-    #Renters email
-    borrower_id_email = fields.Char(string="Borrower's email",related='borrower_id.email')
+        # Renter of the car
+        borrower_id = fields.Many2one('borrower.details', string="Borrower")
+        #Renters email
+        borrower_id_email = fields.Char(string="Borrower's email",related='borrower_id.email')
 
-     #color
-    color = fields.Char(string="Color")
+        #color
+        color = fields.Char(string="Color", required=True)
 
-    #made by
-    manufacturer = fields.Char(string="Manufacturer", required=True)
+        #made by
+        manufacturer = fields.Char(string="Manufacturer", required=True)
 
-    #license plate
-    license = fields.Char(string="License Plate", required=True)
+        #license plate
+        license = fields.Char(string="License Plate", required=True)
 
-    #year of manufacturing
-    YOM = fields.Integer(string="Year of Manufacturing", required=True)
+        #year of manufacturing
+        YOM = fields.Integer(string="Year of Manufacturing", required=True)
 
-    # #last day of service
-    service = fields.Date(string="Lase Date of Service", required=True)
-    
-    # milage
-    mileage = fields.Float(string="Mileage")
+        # #last day of service
+        service = fields.Date(string="Lase Date of Service", required=True)
+        
+        # milage
+        mileage = fields.Float(string="Mileage", required=True)
 
-    #status
-    status = fields.Selection([("available", "Available"), ("rented", "Rented")],string="Status", help="Availability of the car", default="available", compute="_getStatusUpdated")
+        #status
+        status = fields.Selection([("available", "Available"), ("rented", "Rented")],string="Status", help="Availability of the car", default="available", compute="_getStatusUpdated")
 
+        @api.constrains("mileage", "service", "YOM")
+        def _field_constrains(self):
+                for record in self:
+                        current_date = datetime.now().date()
+                        current_year = current_date.year
+                
+                        if record.YOM > current_year:
+                                raise ValidationError("Year of manufacturing cannot be grater then the current year!")
+                
+                        if record.service < current_date:
+                                raise ValidationError("The car has exceeded the last date of service. Thus it cannot be added. Please try adding a different car which has not reached it last date of service")
+                        
+                        if record.mileage < 1:
+                                raise ValidationError("Mileage cannot be zero or negative")
 
-    @api.depends("borrower_id")
-    def _getStatusUpdated(self):
-        print(self)
-        for record in self:
-            record.status = "available"
-            print(record.borrower_id)
-            if record.borrower_id:
-                record.status = "rented"
+        @api.depends("borrower_id")
+        def _getStatusUpdated(self):
+                for record in self:
+                        record.status = "available"
+                        print(record.borrower_id)
+                        if record.borrower_id:
+                                record.status = "rented"
+                
 
-
-    @api.onchange("model")
-    @api.onchange("color")
-    @api.onchange("manufacturer")
-    @api.onchange("license")
-    def _capitalize_letters(self):
-        for record in self:
-            if record.model:
-                    record.model = record.model.capitalize()
-            if record.color:
-                    record.color = record.color.capitalize()
-            if record.manufacturer:
-                    record.manufacturer = record.manufacturer.capitalize()
-            if record.license:
-                    record.license = record.license.upper()
+        # Capitalizing the record in the model        
+        @api.onchange("license", "model", "color", "manufacturer")
+        def _capitalize_letters(self):
+                for record in self:
+                        if record.model:
+                                record.model = record.model.capitalize()
+                        if record.color:
+                                record.color = record.color.capitalize()
+                        if record.manufacturer:
+                                record.manufacturer = record.manufacturer.capitalize()
+                        if record.license:
+                                record.license = record.license.upper()
 
