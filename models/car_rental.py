bc77fa3fe4320291d923ec1bff74c073dd582eff
Nikhil Kumar <nikhil.kumar@targetintegration.com>
2024-09-04 15:33:51 +0530

    added the validation in the rental model the rental dates, added the action in the kanban view of the car model to directly view the model form

--- models/car_rental.py
+++ models/car_rental.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class car_rental(models.Model):
     _name = "car.rental"
@@ -18,3 +19,14 @@ class car_rental(models.Model):
 
     #cost of rental
     cost = fields.Float(String="Cost of rental per month")
+
+
+
+    @api.constrains("car_id", "rental_start", "rental_end")
+    def _check_availability_of_car(self):
+        for record in self:
+            if record.rental_start >= record.rental_end:
+                raise ValidationError("Start date must be before the end date!")
+
+            # overlapping_rentals = self.search([("car_id")])
+        
