c950555456515f87402294e306a8858c9afa39d1
Nikhil Kumar <nikhil.kumar@targetintegration.com>
2024-09-30 14:03:36 +0530

    email

--- __manifest__.py
+++ __manifest__.py
@@ -7,5 +7,5 @@
     "depends": ["base","sale"],
     "installable": True,
     "application": True,
-    "data": ["data/car_model_sequence.xml", "data/car_model_email_template.xml", "report/sales_order_report_template.xml","report/sales_order_report_template.xml","views/people.xml", "views/car_model_view.xml","views/car_owners_view.xml", "views/borrower_view.xml", "views/set_can_be_purchased_view.xml", "views/setting_view.xml", "report/professional_CRM_custom_header.xml", "report/professional_template.xml", "report/CRM_report.xml","views/menu.xml", "security/ir.model.access.csv"]
+    "data": ["data/car_model_sequence.xml", "data/car_model_email_template.xml", "views/people.xml", "views/car_model_view.xml","views/car_owners_view.xml", "views/borrower_view.xml", "views/set_can_be_purchased_view.xml", "views/setting_view.xml", "report/professional_CRM_custom_header.xml", "report/professional_template.xml", "report/CRM_report.xml","views/menu.xml", "security/ir.model.access.csv"]
 }
